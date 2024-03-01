6b781a7c666e26451d89d69a97dc12a1ecce0974
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-03-01 04:56:55 +0000

    node practice

---
+++ tutorial.js
@@ -0,0 +1,12 @@
+// const sum = (num1, num2) => num1 + num2;
+// const PI = 3.14;
+// class CodeBOLT{
+//     constructor(){
+//         console.log("My Brand Name");
+//     }
+
+// }
+// // module.exports = sum;
+// module.exports.sum = sum;
+// module.exports.CodeBOLT = CodeBOLT;
+// module.exports.PI = PI;
